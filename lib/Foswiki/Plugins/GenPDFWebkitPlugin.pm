b4285dc4cd29fc38692f90569b5eb26244ac2a1c
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/GenPDFWebkitPlugin@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/GenPDFWebkitPlugin.pm
+++ lib/Foswiki/Plugins/GenPDFWebkitPlugin.pm
@@ -20,70 +20,71 @@ package Foswiki::Plugins::GenPDFWebkitPlugin;
 
 use strict;
 
-use Foswiki::Func ();
+use Foswiki::Func    ();
 use Foswiki::Plugins ();
 use Error qw(:try);
 
-our $VERSION = '$Rev: 4419 (2009-07-03) $';
-our $RELEASE = '1.00';
-our $SHORTDESCRIPTION = 'Generate PDF using Webkit';
+our $VERSION           = '$Rev: 4419 (2009-07-03) $';
+our $RELEASE           = '1.00';
+our $SHORTDESCRIPTION  = 'Generate PDF using Webkit';
 our $NO_PREFS_IN_TOPIC = 1;
 
 ###############################################################################
 sub initPlugin {
-  my ($topic, $web, $user, $installWeb) = @_;
+    my ( $topic, $web, $user, $installWeb ) = @_;
 
-  if ($Foswiki::Plugins::VERSION < 2.0) {
-    Foswiki::Func::writeWarning('Version mismatch between ',
-    __PACKAGE__, ' and Plugins.pm');
-    return 0;
-  }
+    if ( $Foswiki::Plugins::VERSION < 2.0 ) {
+        Foswiki::Func::writeWarning( 'Version mismatch between ',
+            __PACKAGE__, ' and Plugins.pm' );
+        return 0;
+    }
 
-  return 1;
+    return 1;
 }
 
 ###############################################################################
 sub completePageHandler {
-  #my($html, $httpHeaders) = @_;
 
-  my $query = Foswiki::Func::getCgiQuery();
-  my $contenttype = $query->param("contenttype") || 'text/html';
+    #my($html, $httpHeaders) = @_;
 
-  # is this a pdf view?
-  return unless $contenttype eq "application/pdf";
+    my $query = Foswiki::Func::getCgiQuery();
+    my $contenttype = $query->param("contenttype") || 'text/html';
 
-  require File::Temp;
-  require Foswiki::Sandbox;
+    # is this a pdf view?
+    return unless $contenttype eq "application/pdf";
 
-  # remove left-overs
-  $_[0] =~ s/([\t ]?)[ \t]*<\/?(nop|noautolink)\/?>/$1/gis;
+    require File::Temp;
+    require Foswiki::Sandbox;
 
-  # create temp files
-  my $htmlFile = new File::Temp(SUFFIX => '.html');
-  my $pdfFile = new File::Temp(SUFFIX => '.pdf');
+    # remove left-overs
+    $_[0] =~ s/([\t ]?)[ \t]*<\/?(nop|noautolink)\/?>/$1/gis;
 
-  # creater html file
-  print $htmlFile "$_[0]";
+    # create temp files
+    my $htmlFile = new File::Temp( SUFFIX => '.html' );
+    my $pdfFile  = new File::Temp( SUFFIX => '.pdf' );
 
-  # create webkit command
-  my $session = $Foswiki::Plugins::SESSION;
-  my $pdfCmd = $Foswiki::cfg{GenPDFWebkitPlugin}{WebkitCmd} || 
-    '/usr/local/bin/wkhtmltopdf -q --enable-plugins --outline --print-media-type %INFILE|F% %OUTFILE|F%';
-  
-  # execute
-  my ($output, $exit) = Foswiki::Sandbox->sysCommand(
-      $pdfCmd, 
-      OUTFILE => $pdfFile->filename,
-      INFILE => $htmlFile->filename,
+    # creater html file
+    print $htmlFile "$_[0]";
+
+    # create webkit command
+    my $session = $Foswiki::Plugins::SESSION;
+    my $pdfCmd  = $Foswiki::cfg{GenPDFWebkitPlugin}{WebkitCmd}
+      || '/usr/local/bin/wkhtmltopdf -q --enable-plugins --outline --print-media-type %INFILE|F% %OUTFILE|F%';
+
+    # execute
+    my ( $output, $exit ) = Foswiki::Sandbox->sysCommand(
+        $pdfCmd,
+        OUTFILE => $pdfFile->filename,
+        INFILE  => $htmlFile->filename,
     );
 
-  local $/ = undef;
+    local $/ = undef;
 
-  if ($exit) {
-    throw Error::Simple("execution of wkhtmltopdf failed ($exit)");
-  }
+    if ($exit) {
+        throw Error::Simple("execution of wkhtmltopdf failed ($exit)");
+    }
 
-  $_[0] = <$pdfFile>;
+    $_[0] = <$pdfFile>;
 }
 
 1;
